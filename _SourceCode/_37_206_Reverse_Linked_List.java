72e1dea82a8e6aec754f77b50fbcab231cedc16b
thebrownbox <conghv123@gmail.com>
2020-09-07 22:05:40 +0700

    update

---
+++ _SourceCode/_37_206_Reverse_Linked_List.java
@@ -0,0 +1,52 @@
+
+// Recursion for Linked List
+
+public class R2_Reverse_Linked_List_02 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        // TH co so
+        if(head == null)
+            return null;
+
+        ListNode nextNode = head.next;
+        // TH 1 node
+        if(nextNode == null)
+            return head;
+
+        // TH TQ
+        ListNode newHead = reverseList(nextNode);
+        nextNode.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public  static void printLinkedList(ListNode head)
+    {
+        while (head != null)
+        {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        n1.next = n2; n2.next = n3; n3.next = n4;
+
+        printLinkedList(n1);
+        ListNode newHead = reverseList(n1);
+        printLinkedList(newHead);
+    }
+}
