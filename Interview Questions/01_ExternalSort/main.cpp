d5c4e03cbb44ca29ee49692cc12169084ef7c8cb
thebrownbox <conghv123@gmail.com>
2021-06-23 22:53:22 +0700

    Update

--- Interview Questions/01_ExternalSort/main.cpp
+++ Interview Questions/01_ExternalSort/main.cpp
@@ -8,22 +8,39 @@
 
 using namespace std;
 //-------- Global Variables -----------
+//! Sample data
+char* pInputFileName = "input.txt";
+char* pOutputFileName = "output.txt";
+string strChunkFilePrefix = "chunk_";
+string strOutputFilePrefix = "output_";
+int RAM_SIZE = 1 << 20; // TEST: Ram=1MB, File=10M
 
-//main functions
-int splitSortThenSave(char *inputFileName, int ramSize);
+/**
+ * Split input file to many chunks (with ramSize).
+ * Sort each chunk and save to file
+ * return: number of chunks
+ **/
+int splitSortThenSave(char *pInputFileName, int ramSize);
+/**
+ * Sort the chunk and save to file
+ */
 int sortThenSaveChunkFile(int index, vector<string*>& chunk);
-// utility functions
+
+
+int kWayMerge(int k, int ramSize, char *pOutputFileName);
+
+int getBatchFromChunk(int batchIndex, vector<string*>& batch, ifstream& trunkFile);
+
+/**
+ * For sort vector<string*>
+ **/
 bool compareStringPointers(string *s1, string *s2);
 
 
 
 int main(int argc, char *argv[])
 {
-    //! Just ignore validate the input for now
-    char* pInputFileName = "input.txt";
-    char* pOutputFileName = "output.txt";
-    int RAM_SIZE = 1 << 20; // TEST: Ram=1MB, File=10M
-
+    //! Ignore validate the input for now
     if(argc > 1){
         pInputFileName = argv[1];
         pOutputFileName = argv[2];
@@ -32,25 +49,12 @@ int main(int argc, char *argv[])
 
     cout << "input(" << pInputFileName << ") output(" << pOutputFileName << ") RamSize(" << RAM_SIZE << ")" << endl;
     
-    splitSortThenSave(pInputFileName, RAM_SIZE);
-
-    const int CHUNK_SIZE = 100;
-    cout << "--- FINISHED! ---" << endl;
-    vector<string*> arr;
-    string *s1 = new string("123");
-    string *s2 = new string("3");
-    string *s3 = new string("111111");
-    string *s4 = new string("1111111");
-    arr.push_back(s1);
-    arr.push_back(s2);
-    arr.push_back(s3);
-    arr.push_back(s4);
-    sort(arr.begin(), arr.end(), compareStringPointers);
-    for (size_t i = 0; i < arr.size(); i++)
-    {
-        cout << *arr[i] << " " << arr[i]->size() <<endl;
+    int numberOfChunks = splitSortThenSave(pInputFileName, RAM_SIZE);
+    if(numberOfChunks > 0) {
+        kWayMerge(numberOfChunks, RAM_SIZE, pOutputFileName);
+    } else {
+        cout << "Error: There is something wrong!" << endl;
     }
-    
 
     return 0;
 }
@@ -62,16 +66,17 @@ bool compareStringPointers(string *s1, string *s2)
     return r < 0 ? true : false;
 }
 
-int splitSortThenSave(char *inputFileName, int maxFileSize)
+int splitSortThenSave(char *pInputFileName, int maxFileSize)
 {
-    ifstream inputStream(inputFileName);
+    ifstream inputStream(pInputFileName);
+    int chunkCount = 0;
 
     if(inputStream.is_open())
     {
         int byteCount = 0;
         string strLine;
         vector<string*> vecChunks;
-        int chunkCount = 0;
+        
         while(getline(inputStream, strLine))
         {
             string *pLine = new string(strLine);
@@ -96,16 +101,19 @@ int splitSortThenSave(char *inputFileName, int maxFileSize)
             sortThenSaveChunkFile(chunkCount, vecChunks);
             vecChunks.clear();
         }
+
+        inputStream.close();
+    
+        return chunkCount;
     }
     
-    inputStream.close();
-    return 0;
+    return -1;
 }
 
 int sortThenSaveChunkFile(int index, vector<string*>& chunk)
 {
     stringstream ss;
-    ss << "temp_" << index << ".txt";
+    ss << strChunkFilePrefix << index << ".txt";
     string strFileName = ss.str();
     ofstream outputStream(&strFileName[0]);
 
@@ -120,5 +128,39 @@ int sortThenSaveChunkFile(int index, vector<string*>& chunk)
 
     cout << strFileName << " created! " << endl;
     
+    return 0;
+}
+
+int kWayMerge(int k, int ramSize, char *pOutputFileName)
+{
+    if(k == 1){
+        stringstream chunkFile;
+        chunkFile << strChunkFilePrefix << k << ".txt";
+        rename(&chunkFile.str()[0], pOutputFileName);
+        return 0;
+    }
+
+    // Canculate number of byte should be use in each chunk
+    const int BUFFER_SIZE = ramSize / k;
+    const int CHUNK_BATCH_LIMIT = (ramSize - BUFFER_SIZE) / k;
+    cout << "BUFFER_SIZE: " << BUFFER_SIZE << endl;
+    cout << "CHUNK_BATCH_LIMIT: " << CHUNK_BATCH_LIMIT << endl;
+
+    ifstream* pInputStreamArr = new ifstream[k];
+    vector<string*> *pVecArr = new vector<string*>[k];
+    int* batchIndexArr = new int[k];
+    for (size_t i = 0; i < k; i++)
+    {
+        batchIndexArr[i] = 0;
+    }
+    
+
+    delete[] pInputStreamArr;
+    delete[] pVecArr;
+    delete[] batchIndexArr;
+}
+
+int getBatchFromChunk(int batchIndex, vector<string*>& batch, ifstream& trunkFile)
+{
     return 0;
 }
