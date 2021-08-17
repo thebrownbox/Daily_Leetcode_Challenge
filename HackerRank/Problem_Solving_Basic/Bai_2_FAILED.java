3b0af663f781ae5071bc60a66ba1119fb4aa7a51
thebrownbox <conghv123@gmail.com>
2021-08-17 22:12:35 +0700

    update

--- HackerRank/Problem_Solving_Basic/Bai_2_FAILED.java
+++ HackerRank/Problem_Solving_Basic/Bai_2_FAILED.java
@@ -1,58 +1,84 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Bai2 {
-    private static int[] totalSizeArr = null;
-    private static List<Integer> mFileSize;
-    private static List<Integer> mParent;
-    private static Map<Integer, List<Integer> > childrenMap = new HashMap<>();
+public class Bai_2_FAILED {
+    private static ArrayList<Integer>[] childrenList;
+    private static int[] mFolderSizes;
+    private static List<Integer> mFileSizeList;
+    private static int MIN_DIFF = Integer.MAX_VALUE;
 
-    private static void recalculateSize(int folderIndex, int addSize){
-        int parentIndex = mParent.get(folderIndex);
-        if(parentIndex >= 0)
-        {
-            totalSizeArr[folderIndex] += addSize;
-            recalculateSize(parentIndex, addSize);
+    // Update size của từng folder một | Trả về size của folder hiện tại
+    private static int updateFolderSizeDFS(int curFolderIndex){
+        // Nếu là lá thì update đúng giá trị của nó
+        var myChildren = childrenList[curFolderIndex];
+        if(myChildren == null){
+            mFolderSizes[curFolderIndex] = mFileSizeList.get(curFolderIndex);
+        }else{
+            // Tính toán các note con rồi cộng lại
+            int sumChildrenSize = 0;
+            for (Integer childIndex : myChildren) {
+                sumChildrenSize += updateFolderSizeDFS(childIndex);
+            }
+            mFolderSizes[curFolderIndex] = sumChildrenSize + mFileSizeList.get(curFolderIndex);
         }
+        return mFolderSizes[curFolderIndex];
     }
 
-
     public static int mostBalancedPartition(List<Integer> parent, List<Integer> files_size) {
-        // Write your code here
-        mFileSize = files_size;
-        mParent = parent;
         int n = parent.size();
-        totalSizeArr = new int[n];
+        childrenList = new ArrayList[n];
+        mFolderSizes = new int[n];
+        mFileSizeList = files_size;
+        int rootIndex = -1;
 
-        for (int childIndex = 0; childIndex < n; childIndex++) {
+        //B1: Cập nhật children của các phần tử
+        for (int childIndex = 0; childIndex < childrenList.length; childIndex++) {
             int parentIndex = parent.get(childIndex);
-            List<Integer> chidrenList = childrenMap.get(parentIndex);
-            if(chidrenList == null){
-                chidrenList = new ArrayList<>();
-                chidrenList.add(childIndex);
-                childrenMap.put(parentIndex, chidrenList);
+            if(parentIndex >= 0){
+                if(childrenList[parentIndex] == null){
+                    childrenList[parentIndex] = new ArrayList<>();
+                }
+                childrenList[parentIndex].add(childIndex);
             }else{
-                chidrenList.add(childIndex);
+                rootIndex = childIndex;
             }
-            totalSizeArr[childIndex] = files_size.get(childIndex);
+
+        }
+
+        //B2: Tính lại file size của từng folder
+        updateFolderSizeDFS(rootIndex);
+
+        //B3: DFS tìm xem đoạn nào có thể cắt được
+        int totalSize = 0;
+        for (int i = 0; i < n; i++) {
+            totalSize += files_size.get(i);
         }
 
         for (int i = 0; i < n; i++) {
-            if(childrenMap.get(i) == null){
-                recalculateSize(i, mFileSize.get(i));
-            }
+            int otherSize = totalSize - mFolderSizes[i];
+            MIN_DIFF = Math.min(MIN_DIFF, Math.abs(otherSize - mFolderSizes[i]));
         }
 
-
-        return 0;
+        return MIN_DIFF;
     }
 
     public static void main(String[] args) {
-        Integer[] a = { -1, 0, 1, 2 };
-        Integer[] fileSize = { 1, 4, 3, 4 };
+        // testcase 1
+        // Integer[] a = { -1, 0, 1, 2 };
+        // Integer[] fileSize = { 1, 4, 3, 4 };
+
+        // testcase 2
+        // Integer[] a = { -1, 0, 0, 0 };
+        // Integer[] fileSize = { 10, 11, 10, 10 };
+
+        // testcase 2
+        Integer[] a = { -1, 0, 0, 1, 1, 2 };
+        Integer[] fileSize = { 1, 2, 2, 1, 1, 1 };
+
         mostBalancedPartition(Arrays.asList(a), Arrays.asList(fileSize));
     }
 }
